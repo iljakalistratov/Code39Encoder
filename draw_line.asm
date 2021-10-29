f1bec8edb7ed798d09c32cea0165c63c0c944c48
iljakalistratov <71067944+iljakalistratov@users.noreply.github.com>
2021-10-29 14:37:26 +0200

    Create draw_line.asm

---
+++ draw_line.asm
@@ -0,0 +1,15 @@
+.include "cesplib_rars.asm"
+
+drawline:
+	li a1, 20
+	li a2, 1
+	li a3, 0xffffff
+	line:
+	jal draw_pixel	
+	addi a1, a1,-1
+	bne zero, a1, line
+	
+li a7, 10
+ecall
+
+.include "draw_pixel.asm"		
