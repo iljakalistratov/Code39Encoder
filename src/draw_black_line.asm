942d4b464ef5ac3f0edd3f4893952c7cc2e515c1
iljakalistratov <71067944+iljakalistratov@users.noreply.github.com>
2021-10-30 13:44:44 +0200

    Create draw_black_line.asm

---
+++ src/draw_black_line.asm
@@ -0,0 +1,15 @@
+.include "cesplib_rars.asm"
+
+drawline:
+	li a1, 64
+	li a2, 0
+	li a3, 0x000000
+	line:
+	jal draw_pixel	
+	addi a1, a1,-1
+	bne zero, a1, line
+	
+li a7, 10
+ecall
+
+.include "draw_pixel.asm"
